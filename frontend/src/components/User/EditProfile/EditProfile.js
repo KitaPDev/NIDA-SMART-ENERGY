296ee117399003d88c0f459b6c82e8f7d69bf86b
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2021-06-22 23:39:25 +0700

    WIP image picker

--- frontend/src/components/User/EditProfile/EditProfile.js
+++ frontend/src/components/User/EditProfile/EditProfile.js
@@ -1,4 +1,8 @@
 import React from "react";
+import FormData from "form-data";
+import { ImagePicker } from "react-file-picker";
+import { Row, Col, Container, Button } from "reactstrap";
+import "./EditProfile.css";
 
 class EditProfile extends React.Component {
 	constructor(props) {
@@ -9,10 +13,41 @@ class EditProfile extends React.Component {
 			userType: "",
 			activatedDate: "",
 			lastLogin: "",
+			image: "",
 		};
 	}
+
+	componentDidUpdate(e) {
+		console.log(e);
+	}
+
 	render() {
-		return <div>Edit Profile</div>;
+		return (
+			<div>
+				<Container fluid>
+					<Row className="heading">User Profile</Row>
+					<Row>
+						<Col sm={2}>
+							<ImagePicker
+								extensions={["jpg", "png", "jpeg", "JPG", "PNG", "JPEG"]}
+								dims={{
+									minWidth: 100,
+									maxWidth: 500,
+									minHeight: 100,
+									maxHeight: 500,
+								}}
+								onChange={(base64) => this.setState({ image: base64 })}
+								onError={(err) => {
+									alert(err);
+								}}
+							>
+								<Button>Upload an Image</Button>
+							</ImagePicker>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
 	}
 }
 
