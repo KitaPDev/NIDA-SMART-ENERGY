4cb41044cfc18aa8b90d24606944f74e1fa7e92e
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2021-07-06 11:56:48 +0700

    WIP: device manager table

--- frontend/src/components/User/DeviceManager/DeviceManager.js
+++ frontend/src/components/User/DeviceManager/DeviceManager.js
@@ -1,16 +1,104 @@
 import React from "react";
 import "./DeviceManager.css";
-import { Col, Row, Table } from "reactstrap";
+import { Col, Row, Table, Container } from "reactstrap";
 import { RiFileExcel2Fill } from "react-icons/ri";
-import BootstrapTable from "react-bootstrap-table-next";
 
 class DeviceManager extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			lsDevice: [],
+			isSortByMeterIDAsc: false,
+			isSortByMeterIDDesc: false,
+			isSortByStatusActive: false,
+			isSortByStatusInactive: false,
+		};
+
+		this.toggleSortByMeterID = this.toggleSortByMeterID.bind(this);
+		this.toggleSortByStatus = this.toggleSortByStatus.bind(this);
+	}
+
+	toggleSortByMeterID() {
+		let { isSortByMeterIDAsc, isSortByMeterIDDesc } = this.state;
+
+		if (!(isSortByMeterIDAsc || isSortByMeterIDDesc)) {
+			this.setState({
+				isSortByMeterIDAsc: true,
+				isSortByMeterIDDesc: false,
+				isSortByStatusActive: false,
+				isSortByStatusInactive: false,
+			});
+		} else if (isSortByMeterIDAsc) {
+			this.setState({
+				isSortByMeterIDAsc: false,
+				isSortByMeterIDDesc: true,
+				isSortByStatusActive: false,
+				isSortByStatusInactive: false,
+			});
+		} else if (isSortByMeterIDDesc) {
+			this.setState({
+				isSortByMeterIDAsc: true,
+				isSortByMeterIDDesc: false,
+				isSortByStatusActive: false,
+				isSortByStatusInactive: false,
+			});
+		}
+	}
+
+	toggleSortByStatus() {
+		let { isSortByStatusActive, isSortByStatusInactive } = this.state;
+
+		if (!(isSortByStatusActive || isSortByStatusInactive)) {
+			this.setState({
+				isSortByStatusActive: true,
+				isSortByStatusInactive: false,
+				isSortByMeterIDAsc: false,
+				isSortByMeterIDDesc: false,
+			});
+		} else if (isSortByStatusActive) {
+			this.setState({
+				isSortByStatusActive: false,
+				isSortByStatusInactive: true,
+				isSortByMeterIDAsc: false,
+				isSortByMeterIDDesc: false,
+			});
+		} else if (isSortByStatusInactive) {
+			this.setState({
+				isSortByStatusActive: true,
+				isSortByStatusInactive: false,
+				isSortByMeterIDAsc: false,
+				isSortByMeterIDDesc: false,
+			});
+		}
 	}
 
 	render() {
+		let {
+			lsDevice,
+			isSortByMeterIDAsc,
+			isSortByMeterIDDesc,
+			isSortByStatusActive,
+			isSortByStatusInactive,
+		} = this.state;
+
+		if (isSortByMeterIDAsc) {
+			lsDevice.sort((a, b) =>
+				a.meter_id > b.meter_id ? 1 : b.meter_id > a.meter_id ? -1 : 0
+			);
+		} else if (isSortByMeterIDDesc) {
+			lsDevice.sort((a, b) =>
+				a.meter_id > b.meter_id ? -1 : b.meter_id > a.meter_id ? 1 : 0
+			);
+		} else if (isSortByStatusActive) {
+			lsDevice.sort((a, b) =>
+				a.is_active === 1 ? -1 : a.is_active === 0 ? 1 : 0
+			);
+		} else if (isSortByStatusInactive) {
+			lsDevice.sort((a, b) =>
+				a.is_active === 0 ? -1 : a.is_active === 1 ? 1 : 0
+			);
+		}
+
 		return (
 			<div className="div-device-manager">
 				<Row className="row-heading">
@@ -22,21 +110,48 @@ class DeviceManager extends React.Component {
 					</Col>
 					<Col sm={8}></Col>
 				</Row>
-				<Row className="row-table">
-					<Table responsive>
+				<Container className="container-table-device-manager">
+					<Table className="table-device-manager">
 						<thead className="device-table-head">
-							<th>Meter ID</th>
+							<th
+								className={
+									isSortByMeterIDAsc
+										? "sort_asc"
+										: isSortByMeterIDDesc
+										? "sort_desc"
+										: "sort"
+								}
+								onClick={this.toggleSortByMeterID}
+							>
+								Meter ID
+							</th>
 							<th>Building</th>
+							<th>Floor</th>
 							<th>Location</th>
 							<th>Site</th>
 							<th>Brand / Model</th>
 							<th>System</th>
-							<th>Status</th>
+							<th
+								className={
+									isSortByStatusActive
+										? "sort_asc"
+										: isSortByStatusInactive
+										? "sort_desc"
+										: "sort"
+								}
+								onClick={this.toggleSortByStatus}
+							>
+								Status
+							</th>
 							<th>Activated Date</th>
 						</thead>
-						<tbody></tbody>
+						<tbody>
+							{lsDevice.map((device, index) => (
+								<td></td>
+							))}
+						</tbody>
 					</Table>
-				</Row>
+				</Container>
 			</div>
 		);
 	}
