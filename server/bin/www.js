b900bb321668b033c1fc689279eef02819daf870
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2021-09-20 17:18:25 +0700

    removed https

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,6 +1,6 @@
-// const http = require("http");
-const https = require("https");
-const fs = require("fs");
+const http = require("http");
+// const https = require("https");
+// const fs = require("fs");
 const app = require("../app");
 const dotenv = require("dotenv");
 const db = require("../database");
@@ -15,16 +15,16 @@ port = process.env.PORT;
 app.set("host", hostname);
 app.set("port", port);
 
-let privateKey = fs.readFileSync("sslcert/server.key", "utf8");
-let certificate = fs.readFileSync("sslcert/server.crt", "utf8");
+// let privateKey = fs.readFileSync("sslcert/server.key", "utf8");
+// let certificate = fs.readFileSync("sslcert/server.crt", "utf8");
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-const sslServer = https.createServer(
-	{ key: privateKey, cert: certificate },
-	app
-);
-sslServer.listen(app.get("port"), async function () {
+// const sslServer = https.createServer(
+// 	{ key: privateKey, cert: certificate },
+// 	app
+// );
+server.listen(app.get("port"), async function () {
 	try {
 		let isDBConnected = await db.isDBConnected();
 
