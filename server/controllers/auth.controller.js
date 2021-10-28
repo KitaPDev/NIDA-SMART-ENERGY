34ea293ba1ca3e4ef500732d6596828baec8cd88
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2021-10-28 16:07:58 +0700

    add domain to clearcookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,92 +5,95 @@ const activityService = require("../services/activity.service");
 const httpStatusCodes = require("http-status-codes").StatusCodes;
 
 async function login(req, res) {
-	try {
-		let body = req.body;
-		let username = body.username;
-		let clearTextPassword = body.password;
+  try {
+    let body = req.body;
+    let username = body.username;
+    let clearTextPassword = body.password;
 
-		if (!username) {
-			return res.status(httpStatusCodes.FORBIDDEN).send("Username is required");
-		}
+    if (!username) {
+      return res.status(httpStatusCodes.FORBIDDEN).send("Username is required");
+    }
 
-		if (!(await userService.usernameExists(username))) {
-			return res
-				.status(httpStatusCodes.UNAUTHORIZED)
-				.send("Username does not exist.");
-		}
+    if (!(await userService.usernameExists(username))) {
+      return res
+        .status(httpStatusCodes.UNAUTHORIZED)
+        .send("Username does not exist.");
+    }
 
-		if (await userService.isDeactivated(username)) {
-			return res
-				.status(httpStatusCodes.UNAUTHORIZED)
-				.send(
-					"Your user has been deactivated. For help, contact an administrator."
-				);
-		}
+    if (await userService.isDeactivated(username)) {
+      return res
+        .status(httpStatusCodes.UNAUTHORIZED)
+        .send(
+          "Your user has been deactivated. For help, contact an administrator."
+        );
+    }
 
-		if (username !== "Super Admin") {
-			if (
-				!(await userService.isEmailVerified(
-					await userService.getEmailFromUsername(username)
-				))
-			) {
-				return res
-					.status(httpStatusCodes.UNAUTHORIZED)
-					.send("Email is not verified");
-			}
-		}
+    if (username !== "Super Admin") {
+      if (
+        !(await userService.isEmailVerified(
+          await userService.getEmailFromUsername(username)
+        ))
+      ) {
+        return res
+          .status(httpStatusCodes.UNAUTHORIZED)
+          .send("Email is not verified");
+      }
+    }
 
-		if (!(await authService.verifyPassword(username, clearTextPassword))) {
-			return res.status(httpStatusCodes.UNAUTHORIZED).send("Wrong password.");
-		}
+    if (!(await authService.verifyPassword(username, clearTextPassword))) {
+      return res.status(httpStatusCodes.UNAUTHORIZED).send("Wrong password.");
+    }
 
-		userService.stampLogin(username);
+    userService.stampLogin(username);
 
-		let jwt = await authService.generateJwt(username);
+    let jwt = await authService.generateJwt(username);
 
-		let refreshJwt = await authService.generateRefreshJwt(username);
+    let refreshJwt = await authService.generateRefreshJwt(username);
 
-		activityService.insertActivityUsername(username, 2);
+    activityService.insertActivityUsername(username, 2);
 
-		res.cookie("jwt", jwt, { httpOnly: true, domain: "." + process.env.BASE_DOMAIN });
-		res.cookie("refresh_jwt", refreshJwt, {
-			httpOnly: true,
-			domain: "." + process.env.BASE_DOMAIN
-		});
+    res.cookie("jwt", jwt, {
+      httpOnly: true,
+      domain: "." + process.env.BASE_DOMAIN,
+    });
+    res.cookie("refresh_jwt", refreshJwt, {
+      httpOnly: true,
+      domain: "." + process.env.BASE_DOMAIN,
+    });
 
-		etcService.incrementVisitors();
-		return res.sendStatus(httpStatusCodes.OK);
-	} catch (err) {
-		console.log(err);
-		return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
-	}
+    etcService.incrementVisitors();
+    return res.sendStatus(httpStatusCodes.OK);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
+  }
 }
 
 async function logout(req, res) {
-	try {
-		res.clearCookie("jwt");
-		res.clearCookie("refresh_jwt");
-		res.sendStatus(httpStatusCodes.OK);
-		return;
-	} catch (err) {
-		console.log(err);
-		return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
-	}
+  try {
+    res.clearCookie("jwt", { domain: "." + process.env.BASE_DOMAIN });
+    res.clearCookie("refresh_jwt", { domain: "." + process.env.BASE_DOMAIN });
+    res.sendStatus(httpStatusCodes.OK);
+    return;
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
+  }
 }
 
 async function getUsername(req, res) {
-	try {
-		let username = await authService.getUsernameFromCookies(req);
+  try {
+    let username = await authService.getUsernameFromCookies(req);
 
-		if (username) {
-			return res.status(httpStatusCodes.OK).send(username);
-		}
+    if (username) {
+      return res.status(httpStatusCodes.OK).send(username);
+    }
 
-		return res.sendStatus(httpStatusCodes.FORBIDDEN);
-	} catch (err) {
-		console.log(err);
-		return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
-	}
+    return res.sendStatus(httpStatusCodes.FORBIDDEN);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
+  }
 }
 
 module.exports = { login, logout, getUsername };
--- server/middleware/authenticateJWT.js
+++ server/middleware/authenticateJWT.js
@@ -3,42 +3,44 @@ const authService = require("../services/auth.service");
 const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
-	let cookies = req.cookies;
-	const token = cookies.jwt;
-	if (token) {
-		try {
-			let decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+  let cookies = req.cookies;
+  const token = cookies.jwt;
+  if (token) {
+    try {
+      let decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
 
-			let refreshToken = cookies.refresh_jwt;
+      let refreshToken = cookies.refresh_jwt;
 
-			let newToken;
-			if (jwt.decode(refreshToken) !== null) {
-				newToken = await authService.newToken(refreshToken);
-			}
+      let newToken;
+      if (jwt.decode(refreshToken) !== null) {
+        newToken = await authService.newToken(refreshToken);
+      }
 
-			if (newToken === undefined) {
-				res.clearCookie("jwt");
-				res.clearCookie("refresh_jwt");
-				return res.status(httpStatusCodes.UNAUTHORIZED).send();
-			}
+      if (newToken === undefined) {
+        res.clearCookie("jwt", { domain: "." + process.env.BASE_DOMAIN });
+        res.clearCookie("refresh_jwt", {
+          domain: "." + process.env.BASE_DOMAIN,
+        });
+        return res.status(httpStatusCodes.UNAUTHORIZED).send();
+      }
 
-			res.cookie("jwt", newToken, {
-				httpOnly: true,
-				domain: "." + process.env.BASE_DOMAIN
-			});
+      res.cookie("jwt", newToken, {
+        httpOnly: true,
+        domain: "." + process.env.BASE_DOMAIN,
+      });
 
-			req.userType = decodedToken.userType;
-		} catch (err) {
-			if (err instanceof jwt.TokenExpiredError) {
-				return res.sendStatus(httpStatusCodes.UNAUTHORIZED);
-			}
+      req.userType = decodedToken.userType;
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        return res.sendStatus(httpStatusCodes.UNAUTHORIZED);
+      }
 
-			console.log(err);
-			return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
-		}
+      console.log(err);
+      return res.sendStatus(httpStatusCodes.INTERNAL_SERVER_ERROR);
+    }
 
-		next();
-	} else {
-		return res.status(httpStatusCodes.UNAUTHORIZED).send("No token provided.");
-	}
+    next();
+  } else {
+    return res.status(httpStatusCodes.UNAUTHORIZED).send("No token provided.");
+  }
 };
