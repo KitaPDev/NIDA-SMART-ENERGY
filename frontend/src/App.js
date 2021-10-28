425e01391dbf13bf233a953d1899a244c59a5be7
Kita Pairojtanachai <kita.pairojtanachai@gmail.com>
2021-10-28 20:24:44 +0700

    minor formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,68 +25,68 @@ import i18n from "./i18n";
 import { withTranslation } from "react-i18next";
 
 class App extends React.Component {
-	render() {
-		const { t } = this.props;
+  render() {
+    const { t } = this.props;
 
-		return (
-			<div id="wrapper-root">
-				<div className="locale-selector">
-					<span onClick={() => i18n.changeLanguage("th")}>{t("TH")}</span>|
-					<span onClick={() => i18n.changeLanguage("en")}>{t("ENG")}</span>
-				</div>
-				<Router>
-					<NavBar />
-					<div id="wrapper-content">
-						<ScrollToTop>
-							<Switch>
-								<Route path="/login" component={Login} />
-								<Route path="/forgot-password" component={ForgotPassword} />
-								<Route path="/register" component={Register} />
+    return (
+      <div id="wrapper-root">
+        <div className="locale-selector">
+          <span onClick={() => i18n.changeLanguage("th")}>{t("TH")}</span>|
+          <span onClick={() => i18n.changeLanguage("en")}>{t("ENG")}</span>
+        </div>
+        <Router>
+          <NavBar />
+          <div id="wrapper-content">
+            <ScrollToTop>
+              <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+                <Route path="/register" component={Register} />
 
-								<Route path="/" component={Home} exact />
-								<Route path="/building" component={Building} exact />
-								<Route path="/dashboard" component={Dashboard} exact />
-								<Route path="/report" component={Report} exact />
-								<Route path="/meter" component={Meter} exact />
-								<Route
-									path="/user/edit-profile"
-									component={EditProfile}
-									exact
-								/>
-								<Route
-									path="/user/edit-profile/:username"
-									component={EditProfile}
-									exact
-								/>
-								<Route path="/user/set-target" component={SetTarget} exact />
-								<Route
-									path="/user/device-manager"
-									component={DeviceManager}
-									exact
-								/>
-								<Route
-									path="/user/activity-log"
-									component={ActivityLog}
-									exact
-								/>
-								<Route
-									path="/user/user-management"
-									component={UserManagement}
-									exact
-								/>
-								<Route
-									path="/user/set-permission"
-									component={SetPermission}
-									exact
-								/>
-							</Switch>
-						</ScrollToTop>
-					</div>
-				</Router>
-				<Footer />
-			</div>
-		);
-	}
+                <Route path="/" component={Home} exact />
+                <Route path="/building" component={Building} exact />
+                <Route path="/dashboard" component={Dashboard} exact />
+                <Route path="/report" component={Report} exact />
+                <Route path="/meter" component={Meter} exact />
+                <Route
+                  path="/user/edit-profile"
+                  component={EditProfile}
+                  exact
+                />
+                <Route
+                  path="/user/edit-profile/:username"
+                  component={EditProfile}
+                  exact
+                />
+                <Route path="/user/set-target" component={SetTarget} exact />
+                <Route
+                  path="/user/device-manager"
+                  component={DeviceManager}
+                  exact
+                />
+                <Route
+                  path="/user/activity-log"
+                  component={ActivityLog}
+                  exact
+                />
+                <Route
+                  path="/user/user-management"
+                  component={UserManagement}
+                  exact
+                />
+                <Route
+                  path="/user/set-permission"
+                  component={SetPermission}
+                  exact
+                />
+              </Switch>
+            </ScrollToTop>
+          </div>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default withTranslation()(App);
